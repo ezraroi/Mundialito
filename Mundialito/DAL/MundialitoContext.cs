e3fb8767606e97f8eda9649ab9d9e1541310659a
ezraroi <ezraroi@gmail.com>
2014-02-15 20:28:21 +0200

    Fixed connection strings

--- Mundialito/DAL/MundialitoContext.cs
+++ Mundialito/DAL/MundialitoContext.cs
@@ -11,13 +11,19 @@ namespace Mundialito.DAL
 {
     public class MundialitoContext : IdentityDbContext<MundialitoUser> 
     {
+#if DEBUG
+        private static readonly string connectionString = @"Data Source=(local)\SQLEXPRESS;Initial Catalog=DefaultConnection;Integrated Security=True";
+#else
+        private static readonly string connectionString = @"Server=tcp:m8a4ohtorq.database.windows.net,1433;Database=mundialAGEG1NQnv;User ID=ezraroi@m8a4ohtorq;Password=RoiDror123;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+#endif
+
         public DbSet<Game> Games { get; set; }
         public DbSet<Team> Teams { get; set; }
         public DbSet<Stadium> Stadiums { get; set; }
         public DbSet<Bet> Bets { get; set; }
 
         public MundialitoContext()
-            : base("DefaultConnection")
+            : base(connectionString)
         {
         }
 
