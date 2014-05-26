f8bdce98e229600eb4a2e854a49bd4e4d4d3c95a
ezraroi <ezraroi@gmail.com>
2014-05-27 00:37:16 +0300

    another try

--- Mundialito/TournamentTimesUtils.cs
+++ Mundialito/TournamentTimesUtils.cs
@@ -10,8 +10,8 @@ namespace Mundialito
     public class TournamentTimesUtils
     {
 
-        private static DateTime generalBetsCloseTime = DateTime.Parse(WebConfigurationManager.AppSettings["TournamentStartDate"]).Subtract(TimeSpan.FromDays(1)).ToUniversalTime();
-        private static DateTime generalBetsResolveTime = DateTime.Parse(WebConfigurationManager.AppSettings["TournamentEndDate"]).ToUniversalTime();
+        private static DateTime generalBetsCloseTime = DateTime.Parse("12/6/2014").Subtract(TimeSpan.FromDays(1)).ToUniversalTime();//DateTime.Parse(WebConfigurationManager.AppSettings["TournamentStartDate"]).Subtract(TimeSpan.FromDays(1)).ToUniversalTime();
+        private static DateTime generalBetsResolveTime = DateTime.Parse("13/7/2014").ToUniversalTime();//DateTime.Parse(WebConfigurationManager.AppSettings["TournamentEndDate"]).ToUniversalTime();
 
 
         public static DateTime GeneralBetsCloseTime 
