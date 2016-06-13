2c13267ff941f8e8de7e7b1ac796588c9fa004b9
Roi Ezra <ezraroi@gmail.com>
2016-06-13 19:00:29 +0300

    Extedned access token valid time

--- Mundialito/App_Start/Startup.Auth.cs
+++ Mundialito/App_Start/Startup.Auth.cs
@@ -26,7 +26,7 @@ namespace Mundialito
                 TokenEndpointPath = new PathString("/Token"),
                 Provider = new ApplicationOAuthProvider(PublicClientId, UserManagerFactory),
                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(40),
                 AllowInsecureHttp = true
             };
         }
